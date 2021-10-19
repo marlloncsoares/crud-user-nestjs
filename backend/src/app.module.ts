9134bc217585be5588c298d04e47cdcc0eceb66f
marlloncsoares <marlonmcs@hotmail.com>
2021-10-18 22:59:08 -0300

    feat(app): adiciona recursos para usuários

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from './configs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MongodbConfig } from './configs/config.interface';
 import { DatabaseService } from './database/database.service';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,27 +15,14 @@ import { DatabaseModule } from './database/database.module';
       load: [config],
     }),
 
+    // Configura MongooseModule
     MongooseModule.forRootAsync({
       inject: [DatabaseService],
       imports: [DatabaseModule],
-
       useFactory: (databaseService: DatabaseService) =>
         databaseService.createMongooseOptions(),
     }),
-
-    // Mongoose
-    // MongooseModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: async (configService: ConfigService) => {
-    //     const configMongodbUsers =
-    //       configService.get<MongodbConfig>('mongodbUsers');
-    //     return {
-    //       uri: `mongodb://${configMongodbUsers.username}:${configMongodbUsers.password}@${configMongodbUsers.host}:${configMongodbUsers.port}/${configMongodbUsers.database}`,
-    //       connectionName: 'users',
-    //     };
-    //   },
-    //   inject: [ConfigService],
-    // }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
